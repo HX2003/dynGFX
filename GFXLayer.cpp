565905246bd3a6fe44e57f877e61589e4076ca2b
HX2003 <HX2003@users.noreply.github.com>
2019-12-02 02:38:50 +0800

    Initial

---
+++ GFXLayer.cpp
@@ -0,0 +1,215 @@
+#ifndef _GFXLayer_
+	#include "GFXLayer.h"
+#endif
+Adafruit_GFX* _panel;
+
+void GFXLayer::draw(){
+	GFXLayer* GFXLayer;
+	for(int i = 0; i < ElementList.size(); i++){
+		GFXLayer = ElementList.get(i);
+		GFXLayer->draw();
+	}
+};
+void GFXLayer::setVisibility(bool visibility){
+	GFXLayer* GFXLayer;
+	for(int i = 0; i < ElementList.size(); i++){
+		GFXLayer = ElementList.get(i);
+		GFXLayer->setVisibility(visibility);
+	}
+};
+void GFXLayer::setSimpleOpacity(uint8_t opacity){
+	GFXLayer* GFXLayer;
+	for(int i = 0; i < ElementList.size(); i++){
+		GFXLayer = ElementList.get(i);
+		GFXLayer->setSimpleOpacity(opacity);
+	}
+};
+void GFXLayer::setSimpleOpacityAuto(uint8_t simpleopacity){
+	GFXLayer* GFXLayer;
+	for(int i = 0; i < ElementList.size(); i++){
+		GFXLayer = ElementList.get(i);
+		GFXLayer->setSimpleOpacityAuto(simpleopacity);
+	}
+};
+uint8_t GFXLayer::getSimpleOpacityFirst(){
+	GFXLayer* GFXLayer;
+		GFXLayer = ElementList.get(0);
+		return (GFXLayer->getSimpleOpacityFirst());
+};
+uint16_t GFXLayer::alphaBlendRGB565( uint32_t fg, uint32_t bg, uint8_t alpha){
+    alpha = alpha >> 3;
+    bg = (bg | (bg << 16)) & 0b00000111111000001111100000011111;
+    fg = (fg | (fg << 16)) & 0b00000111111000001111100000011111;
+    uint32_t result = ((((fg - bg) * alpha) >> 5) + bg) & 0b00000111111000001111100000011111;
+    return (uint16_t)((result >> 16) | result);
+}
+//tiled565RGBBitmap Element 
+void tiled565RGBBitmapElement::setVisibility(bool visibility){
+	_visibility=visibility;
+}
+void tiled565RGBBitmapElement::setSimpleOpacity(uint8_t simpleopacity){
+	_simpleopacity=simpleopacity;
+}
+void tiled565RGBBitmapElement::setSimpleOpacityAuto(uint8_t simpleopacity){
+	_simpleopacity=simpleopacity;
+	if(_simpleopacity<=8){
+		_visibility=false;
+	}else if(_simpleopacity>8){
+		_visibility=true;
+	}
+}
+void tiled565RGBBitmapElement::setBackgroundColor(uint16_t backgroundcolor){
+	_backgroundcolor=backgroundcolor;
+}
+void tiled565RGBBitmapElement::drawRGBBitmap(uint16_t x, uint16_t y, const uint16_t *bitmap, uint16_t w, uint16_t h){
+	_x=x;
+	_y=y;
+	_bitmap=bitmap;
+	_w=w;
+	_h=h;
+};
+void tiled565RGBBitmapElement::setCursor(uint16_t cursorx, uint16_t cursory){
+	_cursorx=cursorx;
+	_cursory=cursory;
+}
+uint8_t tiled565RGBBitmapElement::getSimpleOpacityFirst(){
+	return _simpleopacity;
+}
+void tiled565RGBBitmapElement::setTiling(uint16_t tilingx, uint16_t tilingy, uint16_t tilingxmargin, uint16_t tilingymargin){
+	_tilingx=tilingx;
+	_tilingy=tilingy;
+	_tilingxmargin=tilingxmargin;
+	_tilingymargin=tilingymargin;
+}
+void tiled565RGBBitmapElement::draw(){
+	if(_visibility){
+	for(int16_t a=0; a<_tilingx; a++){
+		for(int16_t b=0; b<_tilingy; b++){ 
+			for(int16_t j=0; j<_h; j++) {
+				for(int16_t i=0; i<_w; i++ ) {
+					_panel->writePixel(_x+i+a*(_w+_tilingxmargin)+_cursorx, _y+j+b*(_h+_tilingymargin)+_cursory, alphaBlendRGB565(pgm_read_word(&_bitmap[j * _w + i]),_backgroundcolor,_simpleopacity));
+				}
+			}
+		}
+	}
+	}
+}
+//Rectangle Element
+void rectangleElement::setVisibility(bool visibility){
+	_visibility=visibility;
+}
+void rectangleElement::setSimpleOpacity(uint8_t simpleopacity){
+	_simpleopacity=simpleopacity;
+}
+void rectangleElement::setSimpleOpacityAuto(uint8_t simpleopacity){
+	_simpleopacity=simpleopacity;
+	if(_simpleopacity<=8){
+		_visibility=false;
+	}else if(_simpleopacity>8){
+		_visibility=true;
+	}
+}
+void rectangleElement::setBackgroundColor(uint16_t backgroundcolor){
+	_backgroundcolor=backgroundcolor;
+}
+void rectangleElement::drawRect(uint16_t x, uint16_t y, uint16_t w, uint16_t h, uint16_t rectcolor){
+	_x=x;
+	_y=y;
+	_w=w;
+	_h=h;
+	_rectcolor=rectcolor;
+};
+void rectangleElement::setCursor(uint16_t cursorx, uint16_t cursory){
+	_cursorx=cursorx;
+	_cursory=cursory;
+}
+uint8_t rectangleElement::getSimpleOpacityFirst(){
+	return _simpleopacity;
+}
+void rectangleElement::draw(){
+	if(_visibility){
+	_panel->drawRect(_x,_y,_w,_h, alphaBlendRGB565(_rectcolor,_backgroundcolor,_simpleopacity));
+	}
+}
+//textElement
+void textElement::print(String text){
+	_text=text;
+}
+String textElement::returnVal(){
+	return _text;
+}
+void textElement::setVisibility(bool visibility){
+	_visibility=visibility;
+}
+void textElement::setSimpleOpacity(uint8_t simpleopacity){
+	_simpleopacity=simpleopacity;
+}
+void textElement::setSimpleOpacityAuto(uint8_t simpleopacity){
+	_simpleopacity=simpleopacity;
+	if(_simpleopacity<=8){
+		_visibility=false;
+	}else if(_simpleopacity>8){
+		_visibility=true;
+	}
+}
+void textElement::setFont(const GFXfont *font){
+    _font=font;
+}
+
+void textElement::setTextWrap(bool textwrap){
+	_textwrap=textwrap;
+}
+void textElement::setTextSize(uint8_t textsize){
+	_textsize=textsize;
+}
+void textElement::setBackgroundColor(uint16_t backgroundcolor){
+	_backgroundcolor=backgroundcolor;
+}
+void textElement::setBackgroundColor888(uint8_t r, uint8_t g, uint8_t b){
+	_backgroundcolor=((uint16_t)(r & 0xF8) << 8) | ((uint16_t)(g & 0xFC) << 3) | (b >> 3);
+}
+void textElement::setTextColor(uint16_t textcolor){
+	_textcolor=textcolor;
+}
+void textElement::setTextColor888(uint8_t r, uint8_t g, uint8_t b){
+	_textcolor=((uint16_t)(r & 0xF8) << 8) | ((uint16_t)(g & 0xFC) << 3) | (b >> 3);
+}
+
+void textElement::setCursor(uint16_t cursorx, uint16_t cursory){
+	_cursorx=cursorx;
+	_cursory=cursory;
+}
+uint8_t textElement::getSimpleOpacityFirst(){
+	return _simpleopacity;
+}
+void textElement::draw(void){
+	if(_visibility){
+		char _buf[_text.length()+2];
+		_text.toCharArray(_buf,_text.length()+1);
+		_panel->setFont(_font);
+		_panel->setTextWrap(_textwrap);
+		_panel->setTextSize(_textsize);
+		_panel->setTextColor(alphaBlendRGB565(_textcolor,_backgroundcolor,_simpleopacity));
+		_panel->setCursor(_cursorx,_cursory);
+		_panel->print(_buf);
+		//_panel->display(); use only for matrix
+	}	 
+}
+void GFXLayerInterface::addElement(GFXLayer *element){
+	ElementList.add(element);
+}
+void GFXLayerInterface::draw(){
+	GFXLayer::draw();
+}
+void GFXLayerInterface::setSimpleOpacity(uint8_t opacity){
+	GFXLayer::setSimpleOpacity(opacity);
+};
+void GFXLayerInterface::setSimpleOpacityAuto(uint8_t opacity){
+	GFXLayer::setSimpleOpacityAuto(opacity);	
+};
+void GFXLayerInterface::setVisibility(bool visibility){
+	GFXLayer::setVisibility(visibility);
+}
+uint8_t GFXLayerInterface::getSimpleOpacityFirst(){
+	return GFXLayer::getSimpleOpacityFirst();
+}
---
+++ GFXLayer.h
@@ -0,0 +1,169 @@
+#ifndef _GFXLayer_
+#define _GFXLayer_
+
+#include <Adafruit_GFX.h>
+#include <LinkedList.h>
+
+extern Adafruit_GFX* _panel;
+
+class GFXLayer{
+  public:
+    LinkedList<GFXLayer*> ElementList = LinkedList<GFXLayer*>();
+    GFXLayer(){};
+    ~GFXLayer(){};
+  virtual void 
+   draw(),
+   setVisibility(bool value),
+   setSimpleOpacity(uint8_t opacity),
+   setSimpleOpacityAuto(uint8_t opacity);
+  virtual uint8_t getSimpleOpacityFirst();
+  protected:  
+  uint16_t 
+	alphaBlendRGB565( uint32_t fg, uint32_t bg, uint8_t alpha);
+};
+class tiled565RGBBitmapElement : public GFXLayer{ 
+public:
+	tiled565RGBBitmapElement():GFXLayer(){
+		_bitmap=NULL;
+		_backgroundcolor=0;
+		_cursorx=0;
+		_cursory=0;
+		_x=0;
+		_y=0;
+		_w=0;
+		_h=0;
+		_tilingx=1;
+	    _tilingy=1;
+	    _tilingxmargin=0;
+	    _tilingymargin=0;
+		_visibility=true;
+		_simpleopacity=255;
+	};
+void 
+	drawRGBBitmap(uint16_t x, uint16_t y, const uint16_t *bitmap, uint16_t w, uint16_t h),
+	draw(),
+	setBackgroundColor(uint16_t backgroundcolor),
+	setVisibility(bool value),
+    setSimpleOpacity(uint8_t opacity),
+    setSimpleOpacityAuto(uint8_t opacity),
+	setTiling(uint16_t tilingx, uint16_t tilingy, uint16_t tilingxmargin, uint16_t tilingymargin),
+	setCursor(uint16_t cursorx, uint16_t cursory);
+uint8_t getSimpleOpacityFirst();
+protected:
+	uint8_t  
+	 _simpleopacity;
+    const uint16_t *_bitmap;
+	uint16_t
+	 _backgroundcolor,
+	 _cursorx,
+	 _cursory,
+	 _x,
+	 _y,
+	 _w,
+	 _h,
+	 _tilingx,
+	 _tilingy,
+	 _tilingxmargin,
+	 _tilingymargin;
+	bool
+	 _visibility;
+};
+class rectangleElement : public GFXLayer{ 
+public:
+	rectangleElement():GFXLayer(){
+		_rectcolor=0xFFFF;
+		_backgroundcolor=0;
+		_cursorx=0;
+		_cursory=0;
+		_x=0;
+		_y=0;
+		_w=0;
+		_h=0;
+		_visibility=true;
+		_simpleopacity=255;
+	};
+void 
+	drawRect(uint16_t x, uint16_t y, uint16_t w, uint16_t h, uint16_t rectcolor),
+	draw(),
+	setBackgroundColor(uint16_t backgroundcolor),
+	setVisibility(bool value),
+    setSimpleOpacity(uint8_t opacity),
+    setSimpleOpacityAuto(uint8_t opacity),
+	setCursor(uint16_t cursorx, uint16_t cursory);
+uint8_t getSimpleOpacityFirst();
+protected:
+	uint8_t  
+	 _simpleopacity;
+	uint16_t
+	 _rectcolor,
+	 _backgroundcolor,
+	 _cursorx,
+	 _cursory,
+	 _x,
+	 _y,
+	 _w,
+	 _h;
+	bool
+	 _visibility;
+};
+class textElement : public GFXLayer{ 
+public:
+	textElement():GFXLayer(){
+		_font=NULL;
+		_textwrap=false;
+		_textsize=1;
+		_textcolor=0xFFFF;
+		_backgroundcolor=0;
+		_cursorx=0;
+		_cursory=0;
+		_visibility=true;
+		_simpleopacity=255;
+	};
+	//~textElement();
+void
+	setVisibility(bool value),
+	setSimpleOpacity(uint8_t opacity),
+	setSimpleOpacityAuto(uint8_t opacity),
+	setFont(const GFXfont *font),
+	setTextWrap(bool textwrap),
+	setCursor(uint16_t cursorx, uint16_t cursory),
+	setTextColor(uint16_t textcolor),
+	setTextColor888(uint8_t r, uint8_t g, uint8_t b),
+	setBackgroundColor(uint16_t backgroundcolor),
+	setBackgroundColor888(uint8_t r, uint8_t g, uint8_t b),
+	setTextSize(uint8_t textcolor),
+	print(String text),
+	draw();
+uint8_t getSimpleOpacityFirst();
+String returnVal();
+protected:
+	const GFXfont *_font;
+	uint8_t 
+	 _textsize,
+	 _simpleopacity;
+	uint16_t
+	 _textcolor,
+	 _backgroundcolor,
+	 _cursorx,
+	 _cursory;
+    bool
+	 _textwrap,
+     _visibility;
+	String
+     _text;
+	
+};
+class GFXLayerInterface : public GFXLayer{ 
+  public:
+  GFXLayerInterface(Adafruit_GFX *panel):GFXLayer(){_panel=panel;};
+  //~GFXLayerInterface();
+  void
+	addElement(GFXLayer* element),
+	draw(),
+	setSimpleOpacity(uint8_t opacity),
+	setSimpleOpacityAuto(uint8_t opacity),
+	setVisibility(bool value);
+  uint8_t getSimpleOpacityFirst();
+  protected:
+};
+#endif
